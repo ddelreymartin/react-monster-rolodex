ab39b40c3529065489495730a515e46b35181c23
David <ddelreymartin@stacks-cis.ct>
2020-10-06 15:55:44 +0200

    Updates

---
+++ precache-manifest.663cbbc520a55a27eec9ae64e4131053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874ae40ccffdf9d4f5e8fdcdc3a5080b",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "297464b498045a91946b",
+    "url": "/react-monster-rolodex/static/css/main.bba81d7e.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/react-monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297464b498045a91946b",
+    "url": "/react-monster-rolodex/static/js/main.d79fa094.chunk.js"
+  },
+  {
+    "revision": "8fe39dcbbf8477349779",
+    "url": "/react-monster-rolodex/static/js/runtime-main.3c77c163.js"
+  }
+]);
